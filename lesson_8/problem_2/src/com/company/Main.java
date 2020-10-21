0fb2d9f23e2a35298c75130121c3b1ef4894c946
Shahriyor <shahriyor030387@gmail.com>
2020-10-21 10:34:26 -0700

    + slowed problem 3

--- lesson_8/problem_2/src/com/company/Main.java
+++ lesson_8/problem_2/src/com/company/Main.java
@@ -3,6 +3,15 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.println(sumOfSquares(2,6));
+    }
+
+    public static int sumOfSquares(int x, int y) {
+        int i;
+        int res = 0;
+        for (i = x; i <= y; i++) {
+            res += i * i;
+        }
+        return res;
     }
 }
