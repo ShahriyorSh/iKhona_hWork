499bdde85beb64960d5ae24c15fadbf054f38e32
Shahriyor <shahriyor030387@gmail.com>
2020-10-20 20:41:45 -0700

    slowed problem 2

--- lesson_8/problem_1/src/com/company/Main.java
+++ lesson_8/problem_1/src/com/company/Main.java
@@ -3,5 +3,17 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
+
+        //problem_2
+        int a = maxNumber(14, 120);
+        System.out.println(a);
     }
+
+    public static int maxNumber(int x, int y) {
+        if (x > y) return x;
+        else return y;
+
+    // return Math.max(x, y); idea предлагает это
+    }
+
 }
