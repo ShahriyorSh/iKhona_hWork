db9b950c0fa2c8b20ae13c938e683f3aa7b7f35c
Shahriyor <shahriyor030387@gmail.com>
2020-10-21 11:07:21 -0700

    + problem 6

--- lesson_8/problem_5/src/com/company/Main.java
+++ lesson_8/problem_5/src/com/company/Main.java
@@ -3,7 +3,17 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-            
 
+        int[] someArray = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        System.out.println(sumOfArray(someArray));
     }
+
+    public static int sumOfArray(int[] array) {
+        int res = 0;
+        for (int i = 0; i < array.length; i++) {
+            res += array[i];
+        }
+        return res;
+    }
+
 }
