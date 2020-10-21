2393301e9d0ebe329063b09fcc687a86b3943003
Shahriyor <shahriyor030387@gmail.com>
2020-10-21 13:04:24 -0700

    + slowed class Box

--- lesson_10/box/src/com/company/Main.java
+++ lesson_10/box/src/com/company/Main.java
@@ -3,6 +3,32 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        Box box = new Box();
+        box.height = 40;
+        box.width = 60;
+        box.material = "Plastic";
+        box.weight = 100;
+
+        Box box2 = new Box();
+        box2.height = 20;
+        box2.width = 20;
+        box2.material = "Derevo";
+        box2.weight = 30;
+
+//        System.out.println(box2.height);
+//        System.out.println(box2.width);
+//        System.out.println(box2.material);
+//        System.out.println(box2.weight);
+
     }
+
+
+}
+
+class Box {
+    double height;
+    double width;
+    String material;
+    double weight;
 }
