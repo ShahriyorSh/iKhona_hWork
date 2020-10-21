26e6c628217ee04977f3a8ef0c89385aa32135c8
Shahriyor <shahriyor030387@gmail.com>
2020-10-21 10:55:41 -0700

    + slowed problem 4

--- lesson_8/problem_4/src/com/company/Main.java
+++ lesson_8/problem_4/src/com/company/Main.java
@@ -3,6 +3,18 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
+        int[] array = {1, 2, 3, 4, 5, 88, 5, 4, 3222, 3};
+        System.out.println(getMaxNumberFromArray(array));
 
+    }
+
+    public static int getMaxNumberFromArray(int[] array) {
+        int maxNumber = array[0];
+        for (int i = 0; i < array.length; i++) {
+            if (maxNumber < array[i]) {
+                maxNumber = array[i];
+            }
+        }
+        return maxNumber;
     }
 }
