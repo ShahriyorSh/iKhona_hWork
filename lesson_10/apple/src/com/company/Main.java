7debdb0d788766466fb6e0806cdbb5e6c5ddd687
Shahriyor <shahriyor030387@gmail.com>
2020-10-21 18:52:05 -0700

    slowed Apple class

--- lesson_10/apple/src/com/company/Main.java
+++ lesson_10/apple/src/com/company/Main.java
@@ -3,6 +3,32 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-                    
+
+        Apple apple = new Apple();
+
+        apple.grade = "Vlyams Prayd";
+        apple.color = "Red";
+        apple.sour = "No";
+
+
+        Apple apple2 = new Apple();
+
+        apple2.grade = "Antonovka";
+        apple2.color = "Green";
+        apple2.sour = "Da";
+
+
+    /*    System.out.println("Сорт : " + apple.grade);
+        System.out.println("Колор : " + apple.color);
+        System.out.println("Кислое : " + apple.sour);*/
+
     }
+
+
+}
+
+class Apple {
+    String grade;
+    String color;
+    String sour;
 }
