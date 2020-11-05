8698e97bb0e83367467d303b345a02656a790bbf
Shahriyor <shahriyor030387@gmail.com>
2020-11-05 10:12:29 -0800

    class 2

---
+++ classes/class_2/src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        ProblemTwo pr = new ProblemTwo(1,2);
+        System.out.println(pr.a);
+        System.out.println(pr.b);
+    }
+}
---
+++ classes/class_2/src/com/company/ProblemTwo.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class ProblemTwo {
+
+  public   int a;
+    public int b;
+
+    public ProblemTwo(int aFromCont, int bFromCont) {
+        a = aFromCont;
+        b = bFromCont;
+    }
+}
